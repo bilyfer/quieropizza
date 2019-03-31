bdcdfcb58768478d2eeb685b931db52e9695dbf6
billyafernandez <billy@acklenavenue.com>
2019-03-31 17:08:50 -0600

    Agregando la plantilla para nuestro sitio web publico

--- QuieroPizza/QuieroPizza.Web/Controllers/HomeController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuieroPizza.BL;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,13 @@ namespace QuieroPizza.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = 
+                ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(listadeProductos);
         }
     }
 }
